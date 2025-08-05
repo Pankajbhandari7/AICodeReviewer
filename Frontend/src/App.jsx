aa3cbd4d9008caad9fd1280c6533520d605e7c66
Pankaj Bhandari <bhandaripankaj473@gmail.com>
2025-08-05 16:44:48 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('https://aicodereviewer-backend-y8hh.onrender.com', { code })
+    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
     setReview(response.data)
   }
 
